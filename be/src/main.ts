3f67e389ce8598ad63688e4226287805651b17f6
Jack Gitter <jack.gitter@gmail.com>
2025-09-19 10:34:10 -0400

    ok

--- be/src/main.ts
+++ be/src/main.ts
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/google/auth', (req, res) => {
+    // try to find user in db, if they don't exist create with default permissiosn
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
