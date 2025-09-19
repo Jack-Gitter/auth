fb532627a17fa80e3e79ea56561863a417b67f5a
Jack Gitter <jack.gitter@gmail.com>
2025-09-19 15:13:38 -0400

    ok

--- fe/main.ts
+++ fe/main.ts
@@ -5,9 +5,12 @@ async function main() {
     const app = express()
     const port = 3001
 
-
     app.get('/', (req, res) => {
-        res.send('hi')
+        res.send('<h1>hi</h1>')
+    })
+
+    app.get('/login', (req, res) => {
+        res.send('login')
     })
 
     app.listen(port, () => {
