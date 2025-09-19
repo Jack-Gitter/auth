3e36cf89a97c9a61e09c510153ba1a9c9f683e5d
Jack Gitter <jack.gitter@gmail.com>
2025-09-19 13:57:04 -0400

    ok

--- be/src/auth/google.ts
+++ be/src/auth/google.ts
@@ -1,3 +1,19 @@
+import { dataSource } from "../db/datasource"
+import { User } from "../db/entities/User"
+import jwt from 'jsonwebtoken'
+import { JWTPayload } from "../types"
+
 export async function auth(req, res) {
-    res.send('Hello World')
+    const email = req.params.email
+    const userRepository = dataSource.getRepository(User)
+    const user = await userRepository.findOneBy({email})
+    if (!user) {
+        await userRepository.save([{email}])
+    }
+    const userRoles = user?.roles.map(role => role.type)
+    const jwtPayload: JWTPayload = {
+        roles: userRoles ?? []
+    }
+    const token = jwt.sign(jwtPayload, process.env.JWT_SECRET ?? '')
+    res.send(token)
 }
--- be/src/types.ts
+++ be/src/types.ts
@@ -1,5 +1,10 @@
+
 export enum ROLE_TYPE {
     admin = 'admin',
     manager = 'manager',
     viewer = 'viewer'
+}
+
+export type JWTPayload = {
+    roles: ROLE_TYPE[]
 }
