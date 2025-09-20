17de98b7205f75a51a63bf2443503c2c23273315
Jack Gitter <jack.gitter@gmail.com>
2025-09-20 11:44:10 -0400

    nice

--- be/src/main.ts
+++ be/src/main.ts
@@ -7,6 +7,7 @@ import { addRole } from './role/add-role'
 import { giveRole } from './role/give-role'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 
 async function main() {
@@ -16,12 +17,14 @@ async function main() {
     app.use(bodyParser.urlencoded())
     app.use(bodyParser.json())
     app.use(cookieParser())
+    app.use(cors())
 
     const port = 3000
     await dataSource.initialize()
 
     app.post('/login/id/google', auth)
     app.post('/role/:type', addRole)
+    app.post('/login/access/google', (req, res) => (res.send()))
 
     app.patch('/user/:email/role/:type', giveRole)
 
