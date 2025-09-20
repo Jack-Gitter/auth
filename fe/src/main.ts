bc076ef89eb5f9035275c08105a0186a612a89c2
Jack Gitter <jack.gitter@gmail.com>
2025-09-20 11:23:00 -0400

    nice

--- fe/src/main.ts
+++ fe/src/main.ts
@@ -8,14 +8,15 @@ async function main() {
     const port = 3001
 
     app.get('/', (req, res) => {
-        res.send('<h1>hi</h1>')
+        res.sendFile(__dirname + 'index.html')
     })
 
     app.get('/login/google_id_token', (req, res) => {
         if (req.cookies.jwt) {
             res.redirect('http://localhost:3001')
+        } else {
+            res.sendFile(__dirname + '/login/index.html')
         }
-        res.sendFile(__dirname + '/login/google_id_token/index.html')
     })
     app.get('/login/google_access_token', (req, res) => {
         res.sendFile(__dirname + '/login/google_access_token/index.html')
