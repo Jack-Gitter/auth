7dfea9cf6f10321353f6dec34e79086804b1c05f
Jack Gitter <jack.gitter@gmail.com>
2025-09-21 14:52:15 -0400

    nice

--- be/src/auth/stytch.ts
+++ be/src/auth/stytch.ts
@@ -1,7 +1,22 @@
 import {Request, Response} from 'express'
+import stytch from 'stytch'
 
-export function stytchAuth(req: Request, res: Response) {
+export async function stytchAuth(req: Request, res: Response) {
 
+    const token = req.query.token as string
+
+    const client = new stytch.Client({
+        project_id: process.env.STYTCH_ID ?? '',
+        secret: process.env.STYTCH_SECRET ?? ''
+    });
+
+    const params = {
+      token,
+      session_duration_minutes: 60,
+    };
+
+    const something = await client.magicLinks.authenticate(params)
+    console.log(something)
     res.send()
 
 }
