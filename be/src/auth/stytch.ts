0ba30a1b7319bb111a0f2f7043e06379efaeb94b
Jack Gitter <jack.gitter@gmail.com>
2025-09-21 15:06:23 -0400

    nice

--- be/src/auth/stytch.ts
+++ be/src/auth/stytch.ts
@@ -18,28 +18,36 @@ export async function stytchAuth(req: Request, res: Response) {
       session_duration_minutes: 60,
     };
 
-    const response = await client.magicLinks.authenticate(params)
-    const jwt = response.session_jwt
-    const content = await client.sessions.authenticateJwtLocal({session_jwt: jwt})
-    const userResponse = await client.users.get({user_id: content.user_id})
-    const email = userResponse.emails[0]?.email as string
-    const userRepository = dataSource.getRepository(User)
-    let user = await userRepository.findOne({ where: {email}, relations: ['roles']})
-    if (!user) {
-         user = await userRepository.save({email})
-    }
-    userResponse.roles = userResponse.roles ?? []
-    const roles = user.roles.map(role => role.type)
-    const ourjwt: JWTPayload = {
-        ...content,
-        aud: 'Test Auth',
-        iss: 'Test Auth',
-        sub: content.user_id,
-        roles,
-        authProvider: AUTH_PROVIDER.stytch,
-        accessToken: undefined,
-        refreshToken: undefined
-    }
-    res.cookie('jwt', ourjwt, {maxAge: Date.now() + 60 * 60}).redirect('http://localhost:3001')
+    try {
+        const response = await client.magicLinks.authenticate(params)
+        const jwt = response.session_jwt
+        const content = await client.sessions.authenticateJwtLocal({session_jwt: jwt})
+        const userResponse = await client.users.get({user_id: content.user_id})
+        const email = userResponse.emails[0]?.email as string
+        const userRepository = dataSource.getRepository(User)
+        let user = await userRepository.findOne({ where: {email}, relations: ['roles']})
+        if (!user) {
+             user = await userRepository.save({email})
+        }
+        userResponse.roles = userResponse.roles ?? []
+        const roles = user.roles.map(role => role.type)
+        const ourjwt: JWTPayload = {
+            ...content,
+            aud: 'Test Auth',
+            iss: 'Test Auth',
+            sub: content.user_id,
+            roles,
+            authProvider: AUTH_PROVIDER.stytch,
+            accessToken: undefined,
+            refreshToken: undefined
+        }
+        res.cookie('jwt', ourjwt, {maxAge: Date.now() + 60 * 60}).redirect('http://localhost:3001')
+    } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).send(error.message)
+            } else {
+                res.status(500).send('Something went wrong')
 
+            }
+        }
 }
